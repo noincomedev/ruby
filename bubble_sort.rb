175facc02de15532522792c1a8bd0818d4eefb97
Diego Robles <diego@hemployeed.com>
2019-05-07 10:53:50 -0400

    feat(bubble-sort):pr2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,26 +1,41 @@
-class Bubble_sort
-  attr_accessor :arr
+require 'pry'
+class Project
+   attr_accessor :arr
 
-  def initialize(arr)
-        @arr=arr
-     end
+   def initialize
+   end
 
-     public def todo;
-        @new_arr=@arr
-        for i in 0..@arr.length-1 do
-            for j in 0..@new_arr.length-1 do
-             @item=@arr[i]
-              @to_compare=@new_arr[j]
-                if  @item > @to_compare
-                       @arr[i] = @to_compare
-                       @new_arr[j]=@item
-                end # end of if statement
-             end #end of first for
-         end #end of the second for
-         return @arr
-   end #end del metodo "todo"
-end #end of class
+   public def Bubble_sort(arr);
+      @arr=arr
+      @new_arr=arr
+      for i in 0..@arr.length-1 do
+         for j in 0..@new_arr.length-1 do
+            @item=@arr[i]
+            @to_compare=@new_arr[j]
+            if  @item < @to_compare
+               @arr[i] = @to_compare
+               @new_arr[j]=@item
+            end
+         end
+      end
+      return @arr.to_s
+   end
 
-@Arr=["hi", "hey", "hello", "good-bye"]
-@variable=Bubble_sort.new(@Arr)
-puts @variable.todo
+   def Bubble_sort_by(arr)
+      last_check = arr.length-2
+      count = 0
+      for i in 0..last_check
+         if (yield(arr[i], arr[i + 1])*-1) > 0
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            count += 1
+         end
+      end
+      return arr.reverse.to_s
+   end
+ end
+
+ variable = Project.new
+
+ puts variable.Bubble_sort([4,3,78,2,0,2])
+
+ puts variable.Bubble_sort_by(["hey", "hello", "hi"]) { |left,rigth| r=((left.length - rigth.length))   }
