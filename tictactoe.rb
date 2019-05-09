ff9634891ccd9f73131268df59650bb65540f7da
Diego Robles <diego@hemployeed.com>
2019-05-09 10:56:46 -0400

    feat(tictactoe):add pseudocode logic

---
+++ tictactoe.rb
@@ -0,0 +1,7 @@
+=begin
+1- init game
+2- show rules
+3- start game ? play first ound : close game
+4- unless check winner ? next round : set winner
+5- ask play again ? start game : close game
+=end
