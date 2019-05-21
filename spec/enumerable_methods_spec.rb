05e48a150f70732722908e7f887f05aa9729b1fc
Cristian <inesherald@gmail.com>
2019-05-21 13:17:04 -0500

    ref(testing):add my_count,my_select test methods !

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,4 +1,4 @@
-require "./lib/enumerable_methods.rb"
+require "../lib/enumerable_methods.rb"
 
 RSpec.describe Enumerable do
   describe "#my_each" do
@@ -9,6 +9,16 @@ RSpec.describe Enumerable do
   describe "#my_each_with_index" do
     it "should return each item with index" do
       expect([1,2,3].my_each_with_index{|val,i| [val, i]}).to eql(3)
+    end
+  end
+  describe "#my_count" do
+    it "return the size of the array" do
+      expect([10,6,35,23,10].my_count{ |i| i } ).to eql(5)
+    end
+  end
+  describe "#my_select" do
+    it "obtain the even numbers " do
+      expect([8,18,20,4,30].my_select{|x| x%2==0}).to eql([8,18,20,4,30])
     end
   end
 end
